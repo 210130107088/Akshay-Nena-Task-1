3af25288cbaee4bcba83c87164e81a161536a987
210130107088 <121894245+210130107088@users.noreply.github.com>
2023-04-30 15:16:52 +0530

    Add files via upload

---
+++ to_do_list.js
@@ -0,0 +1,48 @@
+const inputbox = document.getElementById("inputbox");
+const listcontainer = document.getElementById("listcontainer");
+const input = document.getElementById("btn");
+input.addEventListener('keyup', (e) =>{
+    if(e.target.tagName === "LI" ){
+        e.target.classList.toggle("checked");
+        }
+        else if(e.target.tagName === "SPAN"){
+                 e.target.parentElement.remove();
+                 saveData();
+        } 
+});
+
+function addTask(){
+    if(inputbox.value == ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+      inputbox.value = "";
+      saveData();
+}
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI" ){
+        e.target.classList.toggle("checked");
+        }
+        else if(e.target.tagName === "SPAN"){
+                 e.target.parentElement.remove();
+                 saveData();
+        } 
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+
+function showTask(){
+    listcontainer.innerHTML= localStorage.getItem("data");
+
+}
+showTask();
+
